9fe91ab03a0fafa56c23bf04379d4d6e828bb4c5
Pengu-26 <72761159+Pengu-26@users.noreply.github.com>
2020-10-13 14:14:50 -0500

    Update pauline.js

--- pauline.js
+++ pauline.js
@@ -62,7 +62,7 @@ bot.on('message', msg => {
         var message = "*Off the rails!*"
         msg.channel.send(message);
     } else if (msg.content == "p!sendselfie")
-    msg.channel.send("", {files: ["./37B03969-6402-4273-8AEA-A9187512EB2E.jpeg"]})
+    msg.channel.send("Here is my favorite selfie of my me and my BFFs 😄!", {files: ["./37B03969-6402-4273-8AEA-A9187512EB2E.jpeg"]})
 
     } 
     // responses
