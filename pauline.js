708ff224c3d7ee80939e8ab837856caa31aaaceb
Pengu-26 <72761159+Pengu-26@users.noreply.github.com>
2020-10-13 13:05:21 -0500

    Update pauline.js

--- pauline.js
+++ pauline.js
@@ -38,7 +38,7 @@ bot.on('message', msg => {
     else if (msg.content == "p!help") {
         imhelp(msg)
     } else if (msg.content == "p!commands" || msg.content == "p!command") {
-        commands(msg)
+      // commands(msg)
     } else if (msg.content.startsWith("!p")) {
         psay(msg)
     } else if (msg.content == "p!v" || msg.content == "p!version") {
@@ -89,7 +89,7 @@ function commands(msg) {
               "`p!help`\t\t\t\tProvides instructions for how to send messages through Pauline\n" +
               "`!p <msg>`\t\t\tMakes Pauline say message in <msg>\n" +
               "`p!v/version`\tReturns the current version of Pauline\n" +
-             // '`p!ping`\t\t\t\t Pauline responds with "pong"\n' +
+              '`p!ping`\t\t\t\t Pauline responds with "pong"\n' +
               "Ping @Pauline, and she will respond with "🎵PAAAUUULLIIINNEEE🎵\n\n" +
 
               "Ice Mario! will also respond to certain keywords!"
